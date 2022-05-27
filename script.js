756a3356556ea82c799e18f61f91b89cfdc64921
SK627@myscc.ca <SK627@myscc.ca>
2022-05-27 14:57:58 -0400

    Assifnment 1
    Assignment 1

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+	WEB 303 Assignment 1 - jQuery
+	{Simranjit Kaur
+	0781503
+    27/05/2022}
+*/
+  // 6. event handler to run #price value has changed
+     $("#price").on("change", function () {
+ console.log("Simranjit kaur");
+  });
