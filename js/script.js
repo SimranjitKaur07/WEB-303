8bcbd4d4d66c325eafc6d4b3ca5e0857d1b0c6ac
SK627@myscc.ca <SK627@myscc.ca>
2022-06-03 11:26:56 -0400

    %%

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@
 $(document).ready(function(){
     $("#prospect").on("click", function(){        
         $("#data").empty();
-        $("#data").hide().fadeIn(1000);
+        $("#data").hide().fadeIn(100);
         $("#data").load("./prospect.html");
         $("#data").css("display:block");
     });
     $("#convert").on("click", function(){
         $("#data").empty();
-        $("#data").hide().fadeIn(1000);
+        $("#data").hide().fadeIn(100);
         $("#data").load("./convert.html");
         $("#data").css("display:block");
 
     });
     $("#retain").on("click", function(){
         $("#data").empty();
-        $("#data").hide().fadeIn(1000);
+        $("#data").hide().fadeIn(100);
         $("#data").load("./retain.html");
         $("#data").css("display:block");
     });
