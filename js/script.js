a4f66225718acc6f98921ac769f40fb603823974
SK627@myscc.ca <SK627@myscc.ca>
2022-06-03 11:16:03 -0400

    retain

--- js/script.js
+++ js/script.js
@@ -8,5 +8,17 @@ $(document).ready(function(){
         $("#content").load("./prospect.html");
         $("#content").css("display:block");
     });
- 
+    $("#convert").on("click", function(){
+        $("#content").empty();
+        $("#content").hide().fadeIn(1000);
+        $("#content").load("./convert.html");
+        $("#content").css("display:block");
+
+    });
+    $("#retain").on("click", function(){
+        $("#content").empty();
+        $("#content").hide().fadeIn(1000);
+        $("#content").load("./retain.html");
+        $("#content").css("display:block");
+    });
 });
