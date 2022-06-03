81e5d587c0f75109c2ba5f7128c8905cfe83ce5f
SK627@myscc.ca <SK627@myscc.ca>
2022-06-03 11:41:17 -0400

    remove

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@
 $(document).ready(function(){
     $("#prospect").on("click", function(){        
         $("#content-wrapper").empty();
-        $("#content-wrapper").hide().fadeIn(100);
+        $("#content-wrapper").hide();
         $("#content-wrapper").load("./prospect.html");
         $("#content-wrapper").css("display:block");
     });
     $("#convert").on("click", function(){
         $("#content-wrapper").empty();
-        $("#content-wrapper").hide().fadeIn(100);
+        $("#content-wrapper").hide();
         $("#content-wrapper").load("./convert.html");
         $("#content-wrapper").css("display:block");
 
     });
     $("#retain").on("click", function(){
         $("#content-wrapper").empty();
-        $("#content-wrapper").hide().fadeIn(100);
+        $("#content-wrapper").hide();
         $("#content-wrapper").load("./retain.html");
         $("#content-wrapper").css("display:block");
     });
